b19c2518be40606aca2d2ac137cc4c32535afeff
allenaware <allenaware@163.com>
2018-08-22 14:20:14 +0800

    add basic layout

--- src/home/controller/index.js
+++ src/home/controller/index.js
@@ -2,7 +2,7 @@ const Base = require('./base.js');
 
 module.exports = class extends Base {
   indexAction() {
-      var model ={title:'think',body:'JS'};
+      var model ={title:'hello',body:'hello'};
       this.assign('model', model); //给模板赋值
       return this.display();
   }
